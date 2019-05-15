51c41d854b1742e843b8ab5617b04a846ff5d81f
TrueCat17 <truecat17@gmail.com>
2019-05-16 01:48:53 +0300

    add and upd locs

---
+++ resources/images/locations_no_crop_over/q1.py
@@ -0,0 +1,27 @@
+import os
+
+pngs = []
+for d, _, fs in os.walk('.'):
+	for f in fs:
+		if f.endswith('.png'):
+			pngs.append(os.path.join(d, f))
+
+def get_size(f):
+	return os.stat(f).st_size
+
+
+start_sizes = end_sizes = 0
+
+num = 0
+for png in pngs:
+	num += 1
+	print num, '/', len(pngs)
+	
+	s = get_size(png)
+	start_sizes += s
+	os.system('mogrify ' + png)
+	os.system('optipng -strip all -o7 ' + png)
+	e = get_size(png)
+	end_sizes += e
+	
+	print s - e, start_sizes, end_sizes
